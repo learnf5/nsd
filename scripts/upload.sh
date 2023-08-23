87c2f551d39e0022324a8356c271cac60ea9f883
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2023-08-23 14:02:04 -0700

    Update upload.sh
    updated api path to version 9 for R30

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,6 +4,6 @@ HOST=$1
 echo HOST: $HOST
 for EXT in key crt ; do
     CERT=$(sed 's/$/\\n/' $HOST.$EXT | tr -d '\n')
-    URL=https://$HOST/api/8/http/keyvals/ssl_$EXT
+    URL=https://$HOST/api/9/http/keyvals/ssl_$EXT
     echo {\"$HOST\":\""$CERT"\"} | curl -kvH "Content-Type: application/json" -d @- $URL
 done
