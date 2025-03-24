8573654608b9e5c3650b39bb0a18b3fdbfada984
vicki-nginx <48024033+vicki-nginx@users.noreply.github.com>
2025-03-24 15:33:57 -0700

    Update common-tasks.sh

--- common-tasks.sh
+++ common-tasks.sh
@@ -6,6 +6,18 @@ PS4='+$(date +"%T.%3N"): '
 curl --silent https://raw.githubusercontent.com/learnf5/nsd/main/lab-info.md --output /tmp/lab-info.md
 brav_id=$(awk -F '|' "/$LAB_ID/"' {print $2}' /tmp/lab-info.md)
 
+# install 30 day nginx one trial licenses from Salesforce - EXPIRES April 4, 2025
+# In order for NGINX to start & run from R33 and later, MUST have valid jwt license!
+set +x
+#original pathanme - if this works then delete the ones below without the "main" in the pathaname
+curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token $LIC_TOKEN" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-April-4-2025/nginx-one-eval.{crt,key,jwt}
+echo curl --silent --remote-name-all --output-dir /tmp --header "Authorization: token xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" https://raw.githubusercontent.com/learnf5/eval-reg-keys/main/nginx/EXPIRES-April-4-2025/nginx-one-eval.{crt,key,jwt}
+
+set -x
+until sudo scp /tmp/nginx-one-eval.crt nginx:/etc/ssl/nginx/nginx-repo.crt || (( count++ > 5 )); do sleep 5; done
+until sudo scp /tmp/nginx-one-eval.key nginx:/etc/ssl/nginx/nginx-repo.key || (( count++ > 5 )); do sleep 5; done
+until sudo scp /tmp/nginx-one-eval.jwt nginx:/etc/nginx/license.jwt || (( count++ > 5 )); do sleep 5; done
+
 # common files for all labs
 #sudo ssh nginx mkdir --parents /home/ubuntu/public_html/application1
 #curl --silent --remote-name-all --output-dir /tmp https://raw.githubusercontent.com/learnf5/nag/main/common/ubuntu/public_html/index.html
